1276cb9cd0f4760f43d58afbb15658eae4656e11
hypercoffeedude <hypercoffeedude@gmail.com>
2018-08-26 08:45:35 -0400

    Inital Commit

--- SQLite Token Access Test/Program.cs
+++ SQLite Token Access Test/Program.cs
@@ -10,6 +10,22 @@ namespace SQLite_Token_Access_Test
     {
         static void Main(string[] args)
         {
+            // Insert the integer we're searching for here in "LoadToken()".
+            List<TokenModel> returnedToken = SqliteDataAccess.LoadToken(20);
+
+            // DEBUG: Get some feedback to see if it worked.
+            foreach (var item in returnedToken)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine("Integer: " + item.TokenInteger.ToString());
+                Console.WriteLine("Alt String: " + item.TokenAltString);
+                Console.WriteLine("String: " + item.TokenString);
+                Console.WriteLine("ByteHigh: " + item.TokenByteHigh);
+                Console.WriteLine("ByteLow: " + item.TokenByteLow);
+            }
+
+            // Prevent console window from closing. Press enter to exit.
+            Console.Read();
         }
     }
 }
